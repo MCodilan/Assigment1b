95df3a23e13f73b2c56af58521c445da82a9c752
MCodilan <milocee2k@gmail.com>
2016-09-27 10:42:09 -0500

    Assignment1b
    Alternat program for first assigment in CS20S

---
+++ Assigment1/frmHelloUser.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Assigment1
+{
+    partial class frmHelloWorld
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPress = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnPressb = new System.Windows.Forms.Button();
+            this.lblResult2 = new System.Windows.Forms.Label();
+            this.btnPressc = new System.Windows.Forms.Button();
+            this.lblResult3 = new System.Windows.Forms.Label();
+            this.btnPressd = new System.Windows.Forms.Button();
+            this.lblResult4 = new System.Windows.Forms.Label();
+            this.btnPresse = new System.Windows.Forms.Button();
+            this.lblResult5 = new System.Windows.Forms.Label();
+            this.btnPressf = new System.Windows.Forms.Button();
+            this.lblResult6 = new System.Windows.Forms.Label();
+            this.btnPressg = new System.Windows.Forms.Button();
+            this.lblResult7 = new System.Windows.Forms.Label();
+            this.btnPressh = new System.Windows.Forms.Button();
+            this.lblResult8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPress
+            // 
+            this.btnPress.Location = new System.Drawing.Point(37, 29);
+            this.btnPress.Name = "btnPress";
+            this.btnPress.Size = new System.Drawing.Size(119, 23);
+            this.btnPress.TabIndex = 0;
+            this.btnPress.Text = "Do NOT press.";
+            this.btnPress.UseVisualStyleBackColor = true;
+            this.btnPress.Click += new System.EventHandler(this.btnPress_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(37, 80);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 1;
+            // 
+            // btnPressb
+            // 
+            this.btnPressb.Location = new System.Drawing.Point(37, 118);
+            this.btnPressb.Name = "btnPressb";
+            this.btnPressb.Size = new System.Drawing.Size(119, 23);
+            this.btnPressb.TabIndex = 2;
+            this.btnPressb.Text = "Do NOT press.";
+            this.btnPressb.UseVisualStyleBackColor = true;
+            this.btnPressb.Click += new System.EventHandler(this.btnPressb_Click);
+            // 
+            // lblResult2
+            // 
+            this.lblResult2.AutoSize = true;
+            this.lblResult2.Location = new System.Drawing.Point(37, 166);
+            this.lblResult2.Name = "lblResult2";
+            this.lblResult2.Size = new System.Drawing.Size(0, 13);
+            this.lblResult2.TabIndex = 3;
+            // 
+            // btnPressc
+            // 
+            this.btnPressc.Location = new System.Drawing.Point(37, 204);
+            this.btnPressc.Name = "btnPressc";
+            this.btnPressc.Size = new System.Drawing.Size(116, 23);
+            this.btnPressc.TabIndex = 4;
+            this.btnPressc.Text = "Do NOT press.";
+            this.btnPressc.UseVisualStyleBackColor = true;
+            this.btnPressc.Click += new System.EventHandler(this.btnPressc_Click);
+            // 
+            // lblResult3
+            // 
+            this.lblResult3.AutoSize = true;
+            this.lblResult3.Location = new System.Drawing.Point(37, 246);
+            this.lblResult3.Name = "lblResult3";
+            this.lblResult3.Size = new System.Drawing.Size(0, 13);
+            this.lblResult3.TabIndex = 5;
+            // 
+            // btnPressd
+            // 
+            this.btnPressd.Location = new System.Drawing.Point(37, 286);
+            this.btnPressd.Name = "btnPressd";
+            this.btnPressd.Size = new System.Drawing.Size(116, 23);
+            this.btnPressd.TabIndex = 6;
+            this.btnPressd.Text = "Do NOT press.";
+            this.btnPressd.UseVisualStyleBackColor = true;
+            this.btnPressd.Click += new System.EventHandler(this.btnPressd_Click);
+            // 
+            // lblResult4
+            // 
+            this.lblResult4.AutoSize = true;
+            this.lblResult4.Location = new System.Drawing.Point(37, 333);
+            this.lblResult4.Name = "lblResult4";
+            this.lblResult4.Size = new System.Drawing.Size(0, 13);
+            this.lblResult4.TabIndex = 7;
+            // 
+            // btnPresse
+            // 
+            this.btnPresse.Location = new System.Drawing.Point(37, 367);
+            this.btnPresse.Name = "btnPresse";
+            this.btnPresse.Size = new System.Drawing.Size(116, 23);
+            this.btnPresse.TabIndex = 8;
+            this.btnPresse.Text = "PRESS ME";
+            this.btnPresse.UseVisualStyleBackColor = true;
+            this.btnPresse.Click += new System.EventHandler(this.btnPresse_Click);
+            // 
+            // lblResult5
+            // 
+            this.lblResult5.AutoSize = true;
+            this.lblResult5.Location = new System.Drawing.Point(37, 414);
+            this.lblResult5.Name = "lblResult5";
+            this.lblResult5.Size = new System.Drawing.Size(0, 13);
+            this.lblResult5.TabIndex = 9;
+            // 
+            // btnPressf
+            // 
+            this.btnPressf.Location = new System.Drawing.Point(37, 455);
+            this.btnPressf.Name = "btnPressf";
+            this.btnPressf.Size = new System.Drawing.Size(116, 23);
+            this.btnPressf.TabIndex = 10;
+            this.btnPressf.Text = "PRESS ME";
+            this.btnPressf.UseVisualStyleBackColor = true;
+            this.btnPressf.Click += new System.EventHandler(this.btnPressf_Click);
+            // 
+            // lblResult6
+            // 
+            this.lblResult6.AutoSize = true;
+            this.lblResult6.Location = new System.Drawing.Point(37, 504);
+            this.lblResult6.Name = "lblResult6";
+            this.lblResult6.Size = new System.Drawing.Size(0, 13);
+            this.lblResult6.TabIndex = 11;
+            // 
+            // btnPressg
+            // 
+            this.btnPressg.Location = new System.Drawing.Point(37, 549);
+            this.btnPressg.Name = "btnPressg";
+            this.btnPressg.Size = new System.Drawing.Size(116, 23);
+            this.btnPressg.TabIndex = 12;
+            this.btnPressg.Text = "Who\'s There?";
+            this.btnPressg.UseVisualStyleBackColor = true;
+            this.btnPressg.Click += new System.EventHandler(this.btnPressg_Click);
+            // 
+            // lblResult7
+            // 
+            this.lblResult7.AutoSize = true;
+            this.lblResult7.Location = new System.Drawing.Point(37, 603);
+            this.lblResult7.Name = "lblResult7";
+            this.lblResult7.Size = new System.Drawing.Size(0, 13);
+            this.lblResult7.TabIndex = 13;
+            // 
+            // btnPressh
+            // 
+            this.btnPressh.Location = new System.Drawing.Point(37, 646);
+            this.btnPressh.Name = "btnPressh";
+            this.btnPressh.Size = new System.Drawing.Size(131, 23);
+            this.btnPressh.TabIndex = 14;
+            this.btnPressh.Text = "Interupting Dog Wh.....";
+            this.btnPressh.UseVisualStyleBackColor = true;
+            this.btnPressh.Click += new System.EventHandler(this.btnPressh_Click);
+            // 
+            // lblResult8
+            // 
+            this.lblResult8.AutoSize = true;
+            this.lblResult8.Location = new System.Drawing.Point(37, 707);
+            this.lblResult8.Name = "lblResult8";
+            this.lblResult8.Size = new System.Drawing.Size(0, 13);
+            this.lblResult8.TabIndex = 15;
+            // 
+            // frmHelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 748);
+            this.Controls.Add(this.lblResult8);
+            this.Controls.Add(this.btnPressh);
+            this.Controls.Add(this.lblResult7);
+            this.Controls.Add(this.btnPressg);
+            this.Controls.Add(this.lblResult6);
+            this.Controls.Add(this.btnPressf);
+            this.Controls.Add(this.lblResult5);
+            this.Controls.Add(this.btnPresse);
+            this.Controls.Add(this.lblResult4);
+            this.Controls.Add(this.btnPressd);
+            this.Controls.Add(this.lblResult3);
+            this.Controls.Add(this.btnPressc);
+            this.Controls.Add(this.lblResult2);
+            this.Controls.Add(this.btnPressb);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnPress);
+            this.Name = "frmHelloWorld";
+            this.Text = "Hello User";
+            this.Load += new System.EventHandler(this.frmHelloWorld_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPress;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnPressb;
+        private System.Windows.Forms.Label lblResult2;
+        private System.Windows.Forms.Button btnPressc;
+        private System.Windows.Forms.Label lblResult3;
+        private System.Windows.Forms.Button btnPressd;
+        private System.Windows.Forms.Label lblResult4;
+        private System.Windows.Forms.Button btnPresse;
+        private System.Windows.Forms.Label lblResult5;
+        private System.Windows.Forms.Button btnPressf;
+        private System.Windows.Forms.Label lblResult6;
+        private System.Windows.Forms.Button btnPressg;
+        private System.Windows.Forms.Label lblResult7;
+        private System.Windows.Forms.Button btnPressh;
+        private System.Windows.Forms.Label lblResult8;
+    }
+}
+
---
+++ Assigment1/frmHelloUser.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assigment1
+{
+    public partial class frmHelloWorld : Form
+    {
+        public frmHelloWorld()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPress_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "Can you read? Here, I'll give you another chance. Dont screw this up.";
+        }
+
+        private void frmHelloWorld_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPressb_Click(object sender, EventArgs e)
+        {
+            lblResult2.Text = "What did I just say!? Last chance, don't be stupid.";
+        }
+
+        private void btnPressc_Click(object sender, EventArgs e)
+        {
+            lblResult3.Text = "I knew you wouldn't listen. That's why the last couple of buttons were fakes! HA TRY ME NOW!!!";
+        }
+
+        private void btnPressd_Click(object sender, EventArgs e)
+        {
+            lblResult4.Text = "Oh my lord... you are like a child, here play with this.";
+        }
+
+        private void btnPresse_Click(object sender, EventArgs e)
+        {
+            lblResult5.Text = "...";
+        }
+
+        private void btnPressf_Click(object sender, EventArgs e)
+        {
+            lblResult6.Text = "Fine, if that doesn't keep you entertained then I've got a joke for you. \n Knock Knock.";
+        }
+
+        private void btnPressg_Click(object sender, EventArgs e)
+        {
+            lblResult7.Text = "Interupting Dog.";
+        }
+
+        private void btnPressh_Click(object sender, EventArgs e)
+        {
+            lblResult8.Text = "BARK BARK BARK BARK BARK BARK BARK BARK BARK!!!";
+        }
+    }
+}
